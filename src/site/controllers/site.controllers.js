1b7d2baad02f1dd504685ba98f428cb8b8c6250f
tiohs <hamiltonsilva551@gmail.com>
2022-11-14 17:48:19 +0100

    update router

--- src/site/controllers/site.controllers.js
+++ src/site/controllers/site.controllers.js
@@ -2,4 +2,32 @@ export default class SiteController {
   static index(request, response) {
     response.render('index');
   }
+
+  static about(request, response) {
+    response.render('about');
+  }
+
+  static blog(request, response) {
+    response.render('blog');
+  }
+
+  static contact(request, response) {
+    response.render('contact');
+  }
+
+  static detail(request, response) {
+    response.render('detail');
+  }
+
+  static premier(request, response) {
+    response.render('premier');
+  }
+
+  static project(request, response) {
+    response.render('project');
+  }
+
+  static voting(request, response) {
+    response.render('votacao');
+  }
 }
--- src/site/site.routes.js
+++ src/site/site.routes.js
@@ -4,5 +4,10 @@ import SiteController from './controllers/site.controllers';
 const router = Router();
 
 router.get('/', SiteController.index);
+router.get('/about', SiteController.about);
+router.get('/contact', SiteController.contact);
+router.get('/detail', SiteController.detail);
+router.get('/premier', SiteController.premier);
+router.get('/voting', SiteController.voting);
 
 export default router;
